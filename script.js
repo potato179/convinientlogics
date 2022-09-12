04eeec7df874bc61d118ad9693767de2709c07dd
홍은표 <44293278+potato179@users.noreply.github.com>
2022-09-12 12:07:52 +0900

    CU 만들려하는데 잘 모르겠음

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function getGSqr(num){
-    window.open(`https://www.cvsnet.co.kr/qr/${btoa(num)}`, "반값택배 수령용 QR코드", "width=500, height=600")
+    window.open(`https://www.cvsnet.co.kr/qr/${btoa(num)}`, "GS25 반값택배 수령용 QR코드", "width=500, height=600")
+}
+
+function getCUqr(num){
+    window.open(`ㅁㄴㅇㄹ`, "CU 끼리택배 수령용 QR코드", "width=500, height=600")
 }
